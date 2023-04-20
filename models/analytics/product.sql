e6337be04316992386a0572bbba11d18c828b0f2
CarsonIT <carsonweb47@gmail.com>
2023-04-20 10:58:09 +0700

    Commit model product

---
+++ models/analytics/product.sql
@@ -0,0 +1,8 @@
+SELECT product.stock_item_id as product_id,
+product.stock_item_name as product_name,
+coalesce(product.brand,'Undefined') as branch_name,
+product.supplier_id,
+supplier.supplier_name
+FROM `vit-lam-data.wide_world_importers.warehouse__stock_items` AS product
+JOIN learn-dbt-384207.learn_dbt.supplier AS supplier
+ON product.supplier_id = supplier.supplier_id
