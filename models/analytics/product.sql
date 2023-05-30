a18e7b68e13fee5f7307d88706a0c4eb58a4a59c
CarsonIT <carsonweb47@gmail.com>
2023-05-30 11:28:54 +0700

    Commit 30_05_2023

--- models/analytics/product.sql
+++ models/analytics/product.sql
@@ -4,5 +4,5 @@ coalesce(product.brand,'Undefined') as branch_name,
 product.supplier_id,
 supplier.supplier_name
 FROM `vit-lam-data.wide_world_importers.warehouse__stock_items` AS product
-JOIN learn-dbt-384207.learn_dbt.supplier AS supplier
+JOIN {{ ref('supplier') }} AS supplier
 ON product.supplier_id = supplier.supplier_id
